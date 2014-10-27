420d9144c0ed2b6f8ac063696a7a03253c498cea
Matt Messmer <staind2523@gmail.com>
2014-10-26 18:12:32 -0700

    Add roll left frames.

--- src/Player.h
+++ src/Player.h
@@ -21,7 +21,7 @@ class Player : public Sprite {
         float xAccel;
         float yAccel;
 
-        std::vector<SDL_Rect> anims[36];
+        std::vector<SDL_Rect> anims[38];
         bool isAnimLocked;
         std::vector<SDL_Rect*> topAlignedFrames;
 
